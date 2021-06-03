8af24800c3a2f4a693f5521912137d56aa299a01
tomer <tomerbv@gmail.com>
2021-06-03 12:39:05 +0300

    Initial

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -1,4 +1,32 @@
 package View;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
 public class MyViewController implements IView{
+    public MazeGenerator generator;
+    public TextField textField_mazeRows;
+    public TextField textField_mazeColumns;
+    public MazeDisplayer mazeDisplayer;
+
+    public void generateMaze(ActionEvent actionEvent) {
+        if(generator == null)
+            generator = new MazeGenerator();
+
+        int rows = Integer.valueOf(textField_mazeRows.getText());
+        int cols = Integer.valueOf(textField_mazeColumns.getText());
+
+        int[][] maze = generator.generateRandomMaze(rows, cols);
+
+        mazeDisplayer.drawMaze(maze);
+    }
+
+    public void solveMaze(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("Solving maze...");
+        alert.show();
+    }
+
+
 }
